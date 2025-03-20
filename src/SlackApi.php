bbc873cdeaf578ae62ad73094b1accc2c5e2cb71
GiovanniMontinOFC <giovannimontinb@gmail.com>
2025-03-20 12:01:58 -0300

    SlackApi - Atualização das configurações de Token e ID do canal - Bloco personalizado para a mensagem adicionado no json.

--- src/SlackApi.php
+++ src/SlackApi.php
@@ -15,8 +15,14 @@ class SlackApi
     public function __construct()
     {
         $this->client = new Client();
-        $this->tokenValue = SetToken::getToken('SLACK_BOT_TOKEN');
-        $this->channelId = SetToken::getToken('SLACK_CHANNEL_ID');
+        $this->tokenValue = SetEnv::getEnv('SLACK_BOT_TOKEN') ?? throw new \Exception("O Token de autenticação do seu Slack App (Bot) não foi encontrado. Confira o arquivo .env");
+        $this->channelId = SetEnv::getEnv('SLACK_CHANNEL_ID') ?? throw new \Exception("O ID do canal do Slack não foi encontrado. Confira o arquivo .env");
+
+    }
+
+    private function getHeaders(): array
+    {
+        return [ "Authorization" => "Bearer {$this->tokenValue}" ];
     }
 
     public function getChannelMembers(): array
@@ -24,21 +30,34 @@ class SlackApi
 
         $response = $this->client->get('https://slack.com/api/conversations.members', [
             'query' => [ 'channel' => $this->channelId ],
-            'headers' => [ "Authorization" => "Bearer {$this->tokenValue} "], 
+            'headers' => $this->getHeaders(),
         ]);
 
         $data = json_decode($response->getBody(), true);
         return $data['members'] ?? [];
     }
 
-    public function enviarMensagem(string $mensagem): void
+    public function sendMessage(string $message): void
     {
         $this->client->post('https://slack.com/api/chat.postMessage', [
+            'headers' => $this->getHeaders(),
             'json' => [
                 'channel' => $this->channelId,
-                'text' => $mensagem,
+                'blocks' => [
+                    [
+                        "type" => "section",
+                        "text" => [
+                            "type" => "mrkdwn",
+                            "text" => $message
+                        ],
+                        "accessory" => [
+                            "type" => "image",
+                            "image_url" => "https://img.freepik.com/fotos-gratis/conceito-de-programacao-de-agenda-do-planejador-de-calendario_53876-133697.jpg?t=st=1742401832~exp=1742405432~hmac=737f3b0fb7f46552c8c89e43885beffb7eab085ce2bb15ae9879d9c06f130856&w=1380",
+                            "alt_text" => "cute cat"
+                        ]
+                    ]
+                ],
             ],
-            'headers' => [ "Authorization" => "Bearer {$this->tokenValue} "], 
         ]);
     }
 
