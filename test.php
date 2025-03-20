cfa444b8a2198f706ccce094dbe749d078c32216
GiovanniMontinOFC <giovannimontinb@gmail.com>
2025-03-20 12:04:24 -0300

    Atualizado os nomes e realizada a chamada do SimpleBot para teste.

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Giovannimontinofc\BotSorteador;
+require __DIR__ . '/vendor/autoload.php';
+
+// // Teste namespace
+// echo "Se cechegou aqui funciona";
+
+// // Teste código para pegar o Token
+// $tokenTeste = SetToken::getToken('SLACK_BOT_TOKEN');
+// echo $tokenTeste;
+
+// // Teste de receber o membros do canal 
+// $teste = new SlackApi();
+// $membros = $teste->getChannelMembers();
+// var_dump( $membros);
+
+// // Teste de enviar mensagem no canal
+// $mensagem = "Olá, este é um teste enviado pelo bot! <@$membros[1]>";
+// $teste->enviarMensagem($mensagem);
+
+$bot = new SimpleBot();
+while (true) {
+    $bot->runBot();
+    sleep(60);
+}
